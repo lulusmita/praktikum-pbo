a26d6dae8c9f7e9c4e1c2d473ecf10f9f48b5049
lulusmita <88651974+lulusmita@users.noreply.github.com>
2023-03-16 20:27:16 +0700

    Add files via upload

---
+++ Praktikum 4/org/poligon/Poligon.java
@@ -0,0 +1,21 @@
+/*
+*Poligon.java 08/03/2023
+*Penulis: Lulus Dwiyan Mita 24060121120029
+*Deskripsi: representasi dasar dari objek poligon (segi banyak)
+*Lab: B1
+*/
+
+package org.poligon;
+
+public class Poligon{
+      protected int jumlahSisi;
+      public Poligon(){
+            
+      }
+      public void setJumlahSisi(int sisi){
+            this.jumlahSisi=sisi;
+      }
+      public int getJumlahSisi(){
+            return this.jumlahSisi;
+        }
+}
