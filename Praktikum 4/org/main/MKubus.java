a6df36f4f5eda8b0bf443b0260359d0e59da8295
lulusmita <88651974+lulusmita@users.noreply.github.com>
2023-03-21 05:20:34 +0700

    Add files via upload

---
+++ Praktikum 4/org/main/MKubus.java
@@ -0,0 +1,21 @@
+/*
+*MKubus.java 15/03/2023
+*Penulis: Lulus Dwiyan Mita 24060121120029
+*Deskripsi: driver kelas untuk poligon, bujur sangkar, dan kubus
+*Lab: B1
+*/
+
+package org.main;
+
+import org.bangundatar.BujurSangkar;
+
+import org.bangunruang.Kubus;
+
+public class MKubus{
+	public static void main(String[] args){
+                BujurSangkar permukaanKubus = new BujurSangkar(4);
+                Kubus cube = new Kubus(permukaanKubus);
+                System.out.printf("Volume kubus: %f\n",cube.hitungVolume());
+                System.out.printf("Luas alas kubus: %f\n",cube.hitungLuasAlas());
+	}
+}
