649143b4c927050a708aff9e965a8ab62c9a9c32
lulusmita <88651974+lulusmita@users.noreply.github.com>
2023-03-21 05:18:43 +0700

    Add files via upload

---
+++ Praktikum 4/org/bangundatar/BujurSangkar.java
@@ -0,0 +1,33 @@
+/*
+*BujurSangkar.java 15/03/2023
+*Penulis: Lulus Dwiyan Mita 24060121120029
+*Deskripsi: representasi dari objek bujur sangkar, turunan kelas poligon
+*Lab: B1
+*/
+package org.bangundatar;
+
+import org.poligon.Poligon;
+
+public class BujurSangkar extends Poligon{
+	private double panjangSisi;
+
+
+	public BujurSangkar(double panjangSisi){
+		super();
+		super.setJumlahSisi(4);
+		this.panjangSisi = panjangSisi;
+    }
+
+public double hitungLuas(){
+	return this.panjangSisi*this.panjangSisi;
+}
+
+public double getPanjangSisi(){
+    return this.panjangSisi;
+}
+
+
+}
+        
+
+        
