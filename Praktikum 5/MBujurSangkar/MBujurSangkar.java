8894249ab7587b0087640e698a39d2432dc01124
lulusmita <88651974+lulusmita@users.noreply.github.com>
2023-03-31 12:58:36 +0700

    Update MBujurSangkar.java

--- Praktikum 5/MBujurSangkar/MBujurSangkar.java
+++ Praktikum 5/MBujurSangkar/MBujurSangkar.java
@@ -16,4 +16,11 @@ class MBujurSangkar{
    }
 }
 //Praktekkan: apa yang terjadi apabila kelas BujurSangkar tidak membuat implementasi metode abstrak yang ada pada BangunDatar? jelaskan ?
+
+//Jika kelas "BujurSangkar" tidak membuat implementasi metode abstrak yang ada pada kelas induknya "BangunDatar",
+//maka kelas "BujurSangkar" juga harus menjadi kelas abstrak, karena itu berarti bahwa kelas tersebut tidak menyediakan 
+//implementasi lengkap dari metode abstrak pada kelas induknya.Selain itu, kelas yang menggunakan kelas "BujurSangkar" tidak dapat menginstansiasi
+//objek dari kelas "BujurSangkar" karena itu merupakan kelas abstrak dan tidak dapat digunakan untuk membuat objek.
+//Oleh karena itu, kelas "BujurSangkar" harus membuat implementasi dari semua metode abstrak yang ada pada kelas induknya
+//agar dapat diinstansiasi dan digunakan oleh kelas lain yang menggunakannya.
    
