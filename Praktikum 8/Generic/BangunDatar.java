fc1a6c19328e52f7762a704d715c92c05fe743b0
lulusmita <88651974+lulusmita@users.noreply.github.com>
2023-05-22 23:36:18 +0700

    Add files via upload

---
+++ Praktikum 8/Generic/BangunDatar.java
@@ -0,0 +1,10 @@
+/*
+*BangunDatar.java 22/05/2023
+*Penulis: Lulus Dwiyan Mita 24060121120029
+*Deskripsi: kelas abstrak untuk bangun datar
+*Lab: B1
+*/
+
+public abstract class BangunDatar{
+     public abstract double hitungKeliling();
+}
