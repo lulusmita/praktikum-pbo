30678f2e2fb91af2fdc0c4167b40c7de67a740ba
lulusmita <88651974+lulusmita@users.noreply.github.com>
2023-05-22 22:19:53 +0700

    Add files via upload

---
+++ Praktikum 8/Collection/ArrayListTest.java
@@ -0,0 +1,26 @@
+/*
+*ArrayListTest.java 22/05/2023
+*Penulis: Lulus Dwiyan Mita 24060121120029
+*Deskripsi: Program penggunaan objek ArrayList sebagai Collection class
+*Lab: B1
+*/
+
+import java.util.ArrayList;
+
+public class ArrayListTest{
+   public static void main(String[] args){
+       //inisialisasi ArrayList yang hanya dapat berisi objek String
+       ArrayList<String>strings = new ArrayList<String>();
+       //menambah elemen
+       strings.add("praktikum");
+       strings.add("collection");
+       strings.add("dan generics");
+       //menghapus elemen
+       strings.remove("praktikum");
+       //iterasi pada keseluruhan ArrayList
+       for(String s : strings){
+            System.out.print(s+" ");
+       }
+    }
+}
+       
